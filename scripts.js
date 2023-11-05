7d30f3e25a796d1bf38cd65877d3a5bffcbcd86d
Aubrianna-1 <aubrianna.t@gmail.com>
2023-11-04 23:38:45 -0500

    Edited Javascript file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+
+window.addEventListener('load', function(){
+
+    let msg = document.querySelector('.message');
+    let email = document.getElementById('email');
+    let btn = document.querySelector('.newsletter .btn');  
+
+        btn.addEventListener('click', (event)=>{
+        event.preventDefault();
+        let info = email.value;
+
+        if (info){
+            msg.textContent = `Thank you! Your email address '${val}' has been added to our mailing list!`;
+        }
+        else{
+            msg.textContent = "Please enter a valid email address";
+        }
+        email.value = '';
+    });
+});
